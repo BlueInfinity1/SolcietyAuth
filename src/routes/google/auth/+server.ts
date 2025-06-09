874bbc3690ee267ecdaeb22eaf7a54dc17603cdc
BlueInfinity1 <koaxxe@gmail.com>
2025-06-09 22:22:44 +0300

    Hard-coded the callback URL for test tryouts

--- src/routes/google/auth/+server.ts
+++ src/routes/google/auth/+server.ts
@@ -8,8 +8,10 @@ export const GET: RequestHandler = () => {
     const oAuth2Client = new google.auth.OAuth2(
         env.GOOGLE_CLIENT_ID,
         env.GOOGLE_CLIENT_SECRET,
-        `${env.BASE_URL}/google/callback`
+        `https://localhost:5173/google/callback`
         );
+
+        // ${env.GOOGLE_BASE_URL}
 
   const url = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
