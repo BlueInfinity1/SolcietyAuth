74db0af5b30d1243ddba96ab5253e4a5394fc037
BlueInfinity1 <koaxxe@gmail.com>
2025-06-10 22:42:12 +0300

    Changed back to https for the callback

--- src/routes/google/auth/+server.ts
+++ src/routes/google/auth/+server.ts
@@ -8,7 +8,7 @@ export const GET: RequestHandler = () => {
     const oAuth2Client = new google.auth.OAuth2(
         env.GOOGLE_CLIENT_ID,
         env.GOOGLE_CLIENT_SECRET,
-        `localhost:5173/google/callback`
+        `https://localhost:5173/google/callback`
         );
 
         // ${env.GOOGLE_BASE_URL}
