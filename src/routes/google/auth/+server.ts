b38f53c335e6eec4c477189d5b3ecc1607cb9b88
BlueInfinity1 <koaxxe@gmail.com>
2025-06-12 00:39:44 +0300

    Update +server.ts for test

--- src/routes/google/auth/+server.ts
+++ src/routes/google/auth/+server.ts
@@ -5,7 +5,29 @@ import { google } from 'googleapis';
 export const GET: RequestHandler = () => {
     console.log("Svelte auth: " + env.GOOGLE_CLIENT_ID);
 
-    const oAuth2Client = new google.auth.OAuth2(
+    const html = `
+    <html>
+      <head><title>Test Message</title></head>
+      <body>
+        <h1>Message sent to opener</h1>
+        <script>
+          console.log("Sending test postMessage to opener");
+          if (window.opener) {
+            window.opener.postMessage('oauth_success', '*');
+          } else {
+            console.warn("No window.opener found");
+          }
+        </script>
+      </body>
+    </html>
+  `;
+
+  return new Response(html, {
+    headers: { 'Content-Type': 'text/html' },
+    status: 200
+  });
+
+    /*const oAuth2Client = new google.auth.OAuth2(
         env.GOOGLE_CLIENT_ID,
         env.GOOGLE_CLIENT_SECRET,
         //`http://localhost:5173/google/callback`
@@ -19,5 +41,5 @@ export const GET: RequestHandler = () => {
     //prompt:      'consent select_account',  // force the consent dialog & account picker each time
     scope: ['https://www.googleapis.com/auth/presentations.readonly']
   });
-  return Response.redirect(url, 302);
+  return Response.redirect(url, 302); */
 };
